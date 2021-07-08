ee36bccc4d108e91b2bcfeb07f3286c5cd6e84c8
Javier Bértoli <jbertoli@curii.com>
2021-07-08 17:57:07 -0300

    17750: update rvm dependencies for centos-7
    Arvados-DCO-1.1-Signed-off-by: Javier Bértoli <jbertoli@curii.com>

--- test/integration/api/controls/packages_spec.rb
+++ test/integration/api/controls/packages_spec.rb
@@ -10,3 +10,40 @@ control 'arvados api package' do
     it { should be_installed }
   end
 end
+
+control 'arvados cli gem' do
+  title 'should be installed'
+
+  describe gem('arvados-cli') do
+    it { should be_installed }
+  end
+end
+
+control 'RVM and dependencies' do
+  title 'should be installed'
+
+  only_if("Forced requirement for RedHat's family") do
+    os.redhat?
+  end
+
+  %w[
+    rubygem-bundler
+    curl
+    gcc
+    git
+    libcurl
+    libcurl-devel
+    pam-devel
+    postgresql-devel
+    libxml2
+    libxml2-devel
+    make
+    python3-devel
+    ruby-devel
+    zlib-devel
+  ].each do |p|
+    describe package(p) do
+      it { should be_installed }
+    end
+  end
+end
