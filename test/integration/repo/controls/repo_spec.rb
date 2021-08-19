dc2314b05197ef3f73b0a7036dbbcba843c4cb6c
Javier Bértoli <jbertoli@curii.com>
2021-08-19 19:24:15 -0300

    Add the RVM gpg keys in the formula to prevent keyserver's errors
    See https://dev.arvados.org/issues/17990#note-7
    refs #17990
    Arvados-DCO-1.1-Signed-off-by: Javier Bértoli <jbertoli@curii.com>

--- test/integration/repo/controls/repo_spec.rb
+++ test/integration/repo/controls/repo_spec.rb
@@ -1,12 +1,26 @@
 # frozen_string_literal: true
 
+# Copyright (C) The Arvados Authors. All rights reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+
 case os[:name]
 when 'centos'
   repo_file = '/etc/yum.repos.d/arvados.repo'
   repo_url = 'baseurl=http://rpm.arvados.org/CentOS/$releasever/os/$basearch/'
-when 'debian', 'ubuntu'
+when 'debian'
   repo_file = '/etc/apt/sources.list.d/arvados.list'
   repo_url = 'deb http://apt.arvados.org/buster buster main'
+when 'ubuntu'
+  repo_file = '/etc/apt/sources.list.d/arvados.list'
+  repo_url = case platform[:release].to_f.truncate
+             when 20
+               'focal'
+             when 18
+               'bionic'
+             when 16
+               'xenial'
+             end
 end
 
 control 'arvados repository' do
