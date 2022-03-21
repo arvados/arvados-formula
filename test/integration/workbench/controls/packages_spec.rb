41d951c810b0abdf4ffabcd65dccad33692fe73d
Javier Bértoli <jbertoli@curii.com>
2022-03-21 19:29:28 -0300

    18642: ruby-bundler should not be installed
    Arvados-DCO-1.1-Signed-off-by: Javier Bértoli <jbertoli@curii.com>

--- test/integration/workbench/controls/packages_spec.rb
+++ test/integration/workbench/controls/packages_spec.rb
@@ -1,5 +1,16 @@
 # frozen_string_literal: true
 
+control 'ruby bundler package' do
+  title 'should not be installed'
+
+  describe package('ruby-bundler') do
+    it { should_not be_installed }
+  end
+  describe package('bundler') do
+    it { should_not be_installed }
+  end
+end
+
 control 'arvados workbench package' do
   title 'should be installed'
 
